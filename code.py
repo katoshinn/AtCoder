7fc2dfe2f12d7e332d0ac9049bd5ee3705341bc2
加藤真也 <katonyonko@yahoo.co.jp>
2022-01-23 18:27:54 +0900

    追加のテストケースを作れるように修正

--- code.py
+++ code.py
@@ -16,6 +16,12 @@ soup = BeautifulSoup(html, "html.parser")
 # 3. Get items you want.
 test_case = soup.select(".lang-ja pre")
 test_case =[t.text for t in test_case[1:]]
+x = '''
+'''
+y = '''
+'''
+additional_case = []
+test_case += additional_case
 
 for __ in range(0,len(test_case),2):
   sys.stdin = io.StringIO(test_case[__])

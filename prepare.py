85f73fab96c9cc7c3065611267968be0ab24bf86
加藤真也 <katonyonko@yahoo.co.jp>
2022-03-12 20:52:32 +0900

    リアルタイム参加用にコード追加

--- prepare.py
+++ prepare.py
@@ -1,10 +1,11 @@
 #以下に設定するABCの回数を指定する
-times = "235"
+times = "243"
 
 import os
 import shutil
 import textfile
  
+real_time='Yes'
 dir_path = 'C:/Users/katonyonko/OneDrive/デスクトップ/ABC{}'.format(times)
 os.mkdir(dir_path)
 problems = ["a", "b", "c", "d"]
@@ -12,7 +13,11 @@ if int(times) > 125:
   problems += ["e", "f"]
 if int(times) > 211:
   problems += ["g", "h"]
-for i in range(len(problems)):
-  shutil.copy('./code.py', dir_path+'/ABC'+times+'_'+problems[i].upper()+'.py')
-  textfile.replace(dir_path+'/ABC'+times+'_'+problems[i].upper()+'.py', 'times=""', 'times="{}"'.format(times))
-  textfile.replace(dir_path+'/ABC'+times+'_'+problems[i].upper()+'.py', 'problem=""', 'problem="{}"'.format(problems[i]))
+if real_time=='Yes':
+  for i in range(len(problems)):
+      shutil.copy('./code_realtime.py', dir_path+'/ABC'+times+'_'+problems[i].upper()+'.py')
+else:
+  for i in range(len(problems)):
+      shutil.copy('./code.py', dir_path+'/ABC'+times+'_'+problems[i].upper()+'.py')
+      textfile.replace(dir_path+'/ABC'+times+'_'+problems[i].upper()+'.py', 'times=""', 'times="{}"'.format(times))
+      textfile.replace(dir_path+'/ABC'+times+'_'+problems[i].upper()+'.py', 'problem=""', 'problem="{}"'.format(problems[i]))

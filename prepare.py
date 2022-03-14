7246b8835d87ac495a39cd5ef771462fcc6e5f08
katonyonko <katonyonko@yahoo.co.jp>
2022-03-15 00:30:59 +0900

    修正

--- prepare.py
+++ prepare.py
@@ -1,24 +1,12 @@
 #以下に設定するABCの回数を指定する
-<<<<<<< HEAD
 times = "076"
-=======
-<<<<<<< HEAD
-times = "243"
-=======
-times = "078"
->>>>>>> 553b59ae5d316d0b5c3047c1a9ed90ac32cddc42
->>>>>>> 463640a80645a6217ceef2a21b5ab858ded55f10
 
 import os
 import shutil
 import textfile
- 
-<<<<<<< HEAD
+
 real_time='Yes'
-dir_path = 'C:/Users/katonyonko/OneDrive/デスクトップ/ABC{}'.format(times)
-=======
 dir_path = 'C:\\Users\\shinykato\\ABC{}'.format(times)
->>>>>>> 553b59ae5d316d0b5c3047c1a9ed90ac32cddc42
 os.mkdir(dir_path)
 problems = ["a", "b", "c", "d"]
 if int(times) > 125:

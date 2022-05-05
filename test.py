b4b5022ec223fd40aae1c8ac7635e7844eaf8316
加藤真也 <katonyonko@yahoo.co.jp>
2022-05-06 05:15:37 +0900

    LCAのところ修正

--- test.py
+++ test.py
@@ -3,36 +3,42 @@ import sys
 
 _INPUT = """\
 6
-12 4 3
-3 1
-6 5
-4 3
-10 3 4
-7 3
-3 1
-5 4
-6 3
+8
+-690377870 113265092 309910313
+-237317502 226530184 111095495
+-428244446 20449344 86035324
+-39706910 368088192 781991
+-719554585 184918601 393468779
+-164798782 369837202 30446065
+-676521103 63096981 401281747
+-171745255 252387924 62120322
+-521613854 56777512 428978993
+-180948782 170332536 141234994
+-831388448 111561549 52842458
+172665493 334684647 8107233
+-521749554 82172757 18542931
+-110885769 328691028 4319665
+-852824253 182038292 292554027
+57367207 364076584 139043300
 """
 
 sys.stdin = io.StringIO(_INPUT)
 case_no=int(input())
 for __ in range(case_no):
-  from heapq import heappop, heappush
-  X,F,N=map(int,input().split())
-  camel=[[0,0]]
-  for i in range(N):
-    x=list(map(int,input().split()))
-    camel.append(x)
-  camel.append([X,0])
-  camel.sort()
-  h=[]
-  nxt=F
-  ans=0
-  for i in range(N+1):
-    nxt+=-camel[i+1][0]+camel[i][0]
-    while nxt<0 and len(h)>0:
-      nxt-=heappop(h)
-      ans+=1
-    if nxt<0: ans=-1; break
-    heappush(h,-camel[i+1][1])
-  print(ans)
+  import math
+  mod=10**9+7
+  t=int(input())
+  for _ in range(t):
+    b,q,y=map(int,input().split())
+    c,r,z=map(int,input().split())
+    if r%q!=0 or c<b or (c-b)%q!=0 or c+(z-1)*r>b+(y-1)*q: print(0)
+    elif c-r<b or c+z*r>b+(y-1)*q: print(-1)
+    else:
+      ans=0
+      i=1
+      while i*i<=r:
+        if r%i==0:
+          if i*q//math.gcd(i,q)==r: ans=(ans+(r//i)**2)%mod
+          if i**2!=r and r//i*q//math.gcd(r//i,q)==r: ans=(ans+i**2)%mod
+        i+=1
+      print(ans)

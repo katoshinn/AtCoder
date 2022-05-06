934516e83079c106afe2c0cbc51740c4e20181c0
katonyonko <katonyonko@yahoo.co.jp>
2022-05-06 16:48:34 +0900

    FordFulkerson追加

--- Algorithm.py
+++ Algorithm.py
@@ -1194,3 +1194,46 @@ def BellmanFord(G,s=0):
     for i in range(len(G)):
       if cycle[i]==1: D[i]='-inf'
     return D
+
+# Ford-Fulkerson algorithm
+class FordFulkerson:
+    def __init__(self, N):
+        self.N = N
+        self.G = [[] for i in range(N)]
+ 
+    def add_edge(self, fr, to, cap):
+        forward = [to, cap, None]
+        forward[2] = backward = [fr, 0, forward]
+        self.G[fr].append(forward)
+        self.G[to].append(backward)
+ 
+    def add_multi_edge(self, v1, v2, cap1, cap2):
+        edge1 = [v2, cap1, None]
+        edge1[2] = edge2 = [v1, cap2, edge1]
+        self.G[v1].append(edge1)
+        self.G[v2].append(edge2)
+ 
+    def dfs(self, v, t, f):
+        if v == t:
+            return f
+        used = self.used
+        used[v] = 1
+        for e in self.G[v]:
+            w, cap, rev = e
+            if cap and not used[w]:
+                d = self.dfs(w, t, min(f, cap))
+                if d:
+                    e[1] -= d
+                    rev[1] += d
+                    return d
+        return 0
+ 
+    def flow(self, s, t):
+        flow = 0
+        f = INF = 10**9 + 7
+        N = self.N 
+        while f:
+            self.used = [0]*N
+            f = self.dfs(s, t, INF)
+            flow += f
+        return flow

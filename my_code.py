2ed52a3e6bc51faf9fa38ef10f5077609b8774be
加藤真也 <katonyonko@yahoo.co.jp>
2025-04-06 18:08:52 +0900

    細かいところを習性

--- my_code.py
+++ my_code.py
@@ -3,51 +3,25 @@ import sys
 from collections import defaultdict, deque, Counter
 from itertools import permutations, combinations, accumulate
 from heapq import heappush, heappop
-sys.setrecursionlimit(10**6)
 from bisect import bisect_right, bisect_left
 from math import gcd
 import math
-import atcoder
 
 _INPUT = """\
-6
 """
 
-def solve(test):
-  ans=0
-  if test==0:
-    print(ans)
-  else:
-    return None
+def input():
+  return sys.stdin.readline()[:-1]
 
-def random_input():
-  from random import randint,shuffle
-  N=randint(1,10)
-  M=randint(1,N)
-  A=list(range(1,M+1))+[randint(1,M) for _ in range(N-M)]
-  shuffle(A)
-  return (" ".join(map(str, [N,M]))+"\n"+" ".join(map(str, A))+"\n")*3
+def solve():
+  pass
 
-def simple_solve():
-  return []
-
-def main(test):
-  if test==0:
-    solve(0)
-  elif test==1:
+def main():
+  if sys.stdin.isatty():
     sys.stdin = io.StringIO(_INPUT)
-    case_no=int(input())
-    for _ in range(case_no):
-      solve(0)
+    while True:
+      solve()
   else:
-    for i in range(1000):
-      sys.stdin = io.StringIO(random_input())
-      x=solve(1)
-      y=simple_solve()
-      if x!=y:
-        print(i,x,y)
-        print(*[line for line in sys.stdin],sep='')
-        break
+    solve()
 
-#0:提出用、1:与えられたテスト用、2:ストレステスト用
-main(1)
+main()
